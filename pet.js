44616271e97009b6063609a2262b127fb818966d
Stuart P. Bentley <s@stuartpb.com>
2019-05-15 00:38:00 +0000

    Require movement to purr

--- pet.js
+++ pet.js
@@ -2,19 +2,32 @@
 // not to fire the first few times they're called, due to page restrictions on
 // what can be triggered without a user event.
 
+var distancesTraveled = new Map();
+var purrDistance = 10;
+
 document.body.addEventListener('pointerdown',function(e){
+  distancesTraveled.set(e.pointerId, 0);
   if (document.fullscreenEnabled && !document.fullscreenElement) {
-      document.body.requestFullscreen().catch(console.info);
+    document.body.requestFullscreen().catch(console.info);
+  }
+  if (screen.orientation) {
+    screen.orientation.lock('portrait').catch(console.info);
   }
 });
 
 document.body.addEventListener('pointermove', function(e) {
-  try {
-    var clampedTopPressureRange = Math.min(Math.max(
-      (e.pressure-0.5) * 2, .1), 1);
-    window.navigator.vibrate(clampedTopPressureRange*10);
+  // backtracking is just not recognized
+  var distance = Math.max(
+    distancesTraveled.get(e.pointerId) + e.movementY, 0);
+  if (distance >= purrDistance) {
+    try {
+      var clampedTopPressureRange = Math.min(Math.max(
+        (e.pressure-0.5) * 2, .1), 1);
+      window.navigator.vibrate(clampedTopPressureRange*10);
+    }
+    catch (err) {
+      console.info(err);
+    }
   }
-  catch (err) {
-    console.info(err);
-  }
+  distancesTraveled.set(e.pointerId, distance % purrDistance);
 });
